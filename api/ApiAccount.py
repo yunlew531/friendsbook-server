5b3d461607a63b1384785e4f865da57168f1815e
Daniel <yunlew531@gmail.com>
2022-10-31 14:46:45 +0800

    開api: 登出

--- api/ApiAccount.py
+++ api/ApiAccount.py
@@ -6,9 +6,18 @@ from collection.User import User
 from flask_bcrypt import Bcrypt
 import os
 import jwt
+import datetime
 bcrypt = Bcrypt()
 
 class ApiAccount(Resource):
+  # logout
+  def get(self):
+    authorization = request.headers.get('Authorization')
+    try:
+      jwt.decode(authorization, os.getenv('JWT_KEY'), algorithms=['HS256'])
+    except jwt.exceptions.DecodeError as e:
+      return { 'message': str(e) }
+    return { 'message': 'success' }
   # login
   def post(self):
     body = request.get_json()
@@ -18,12 +27,13 @@ class ApiAccount(Resource):
     if not password: return { 'message': 'password required.' }, 400
     if len(password) < 6: return { 'message': 'password must be at least 6 characters.' }, 400
 
-    user = json.loads(User.objects(email=email).only('password').first().to_json())
+    user = json.loads(User.objects(email=email).only('password', 'uid').first().to_json())
     if not user: return { 'message': 'user not found.' }, 404
     hash_password = user.get('password')
     is_password_valid = bcrypt.check_password_hash(hash_password, password)
     if not is_password_valid: return { 'message': 'password is wrong.'}, 400
     uid = user.get('uid')
-    jwt_token = jwt.encode({'uid': uid}, os.getenv('JWT_KEY'), algorithm="HS256")
+    jwt_exp = datetime.datetime.now()+ datetime.timedelta(days=7)
+    jwt_token = jwt.encode({'uid': uid, 'exp': jwt_exp}, os.getenv('JWT_KEY'), algorithm="HS256")
     return { 'message': 'success', 'token': jwt_token}
 
